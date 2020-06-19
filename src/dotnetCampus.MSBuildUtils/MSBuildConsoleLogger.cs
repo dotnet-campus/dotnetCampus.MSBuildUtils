0b39216657cc8ce52e228d935dc6c0651ce45a08
walterlv <xblero@gmail.com>
2020-06-19 20:16:51 +0800

    将大量的参数复制到 Logger 类中，这样可以在 Output 的时候少写一个参数，用起来爽一丢丢

--- src/dotnetCampus.MSBuildUtils/MSBuildConsoleLogger.cs
+++ src/dotnetCampus.MSBuildUtils/MSBuildConsoleLogger.cs
@@ -19,14 +19,27 @@ namespace dotnetCampus.MSBuildUtils
             => Output(new MSBuildMessage(message, MessageLevel.Warning, null, filePath, line));
 
         /// <inheritdoc/>
+        public void Warning(string message, string? id = null, string? targetFile = null,
+            int? lineStart = null, int? columnStart = null, int? lineEnd = null, int? columnEnd = null)
+            => Output(new MSBuildMessage(message, MessageLevel.Warning, id, targetFile, lineStart, columnStart, lineEnd, columnEnd));
+
+        /// <inheritdoc/>
         public void Error(string message, [CallerFilePath] string? filePath = null, [CallerLineNumber] int line = 1)
             => Output(new MSBuildMessage(message, MessageLevel.Error, null, filePath, line));
 
         /// <inheritdoc/>
+        public void Error(string message, string? id = null, string? targetFile = null,
+            int? lineStart = null, int? columnStart = null, int? lineEnd = null, int? columnEnd = null)
+            => Output(new MSBuildMessage(message, MessageLevel.Error, id, targetFile, lineStart, columnStart, lineEnd, columnEnd));
+
+        /// <inheritdoc/>
         public void Throw(string message, [CallerFilePath] string? filePath = null, [CallerLineNumber] int line = 1)
-        {
-            throw new MSBuildException(new MSBuildMessage(message, MessageLevel.Error, null, filePath, line));
-        }
+            => throw new MSBuildException(new MSBuildMessage(message, MessageLevel.Error, null, filePath, line));
+
+        /// <inheritdoc/>
+        public void Throw(string message, string? id = null, string? targetFile = null,
+            int? lineStart = null, int? columnStart = null, int? lineEnd = null, int? columnEnd = null)
+            => throw new MSBuildException(new MSBuildMessage(message, MessageLevel.Error, id, targetFile, lineStart, columnStart, lineEnd, columnEnd));
 
         /// <inheritdoc/>
         public void Output(MSBuildMessage message) => Console.WriteLine(message);
